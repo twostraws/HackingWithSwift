98de35d84b3e2adca0e153d812c74133f5126e03
schwalbe10 <schwalbe10@gmail.com>
2017-03-29 22:54:12 +0900

    Fix a typo

--- project39/Project39Tests/Project39Tests.swift
+++ project39/Project39Tests/Project39Tests.swift
@@ -29,7 +29,7 @@ class Project39Tests: XCTestCase {
 	func testWordCountsAreCorrect() {
 		let playData = PlayData()
 		XCTAssertEqual(playData.wordCounts.count(for: "home"), 174, "Home does not appear 174 times")
-		XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 174 times")
+		XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 4 times")
 		XCTAssertEqual(playData.wordCounts.count(for: "mortal"), 41, "Mortal does not appear 41 times")
 	}
 
