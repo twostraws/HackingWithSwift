d514c40921e4bbdee85b296a0a69eefe18f05531
Gaurav Thakkar <thakkargaurav409@gmail.com>
2022-08-16 22:33:09 +0530

    Update AppDelegate.swift
    Remove code to add tabBarItem from AppDelegate

--- Classic/project7/Project7/AppDelegate.swift
+++ Classic/project7/Project7/AppDelegate.swift
@@ -15,12 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-		if let tabBarController = window?.rootViewController as? UITabBarController {
-			let storyboard = UIStoryboard(name: "Main", bundle: nil)
-			let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
-			vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
-			tabBarController.viewControllers?.append(vc)
-		}
+		
 
 		return true
 	}
