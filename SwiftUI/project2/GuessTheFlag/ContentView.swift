c88bb71b7f0015f46ff02ebd3ed4f74fe2b8f769
Ashok DY <ashokd@t6hs.com>
2025-06-02 15:15:19 -0400

    FIxed the score field

--- SwiftUI/project2/GuessTheFlag/ContentView.swift
+++ SwiftUI/project2/GuessTheFlag/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
 
     var body: some View {
         ZStack {
@@ -57,7 +58,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
 
@@ -68,13 +69,14 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
         }
